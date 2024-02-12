f5d66fbb19e569a3387ea72b9e745f826ba0456b
Matheus Santiago <matheustiecher0806@gmail.com>
2024-02-12 19:43:51 -0300

    Adicionado prompt para o usuário escrever o cep e também comentários

--- ConsultaCEP.js
+++ ConsultaCEP.js
@@ -1,10 +1,18 @@
-const apiUrl = 'https://brasilapi.com.br/api/cep/v1/89025301';
+/*Cria variável para solicitar CEP ao usuário*/
+cep = prompt("Qual seu CEP?")
 
+/*Constante para construir a varíavel com o CEP do usuário*/
+const apiUrl = `https://brasilapi.com.br/api/cep/v1/${cep}`;
+
+/*Função para tratar os dados da API e apresentar no console*/
 async function getcep(){
+    
     try{
+    /*Busca response da API e transforma em JSON*/
     const response = await fetch(apiUrl);
     const data = await response.json()
 
+    /*Apresenta os dados no console*/
     console.log('Consulta realizada com sucesso')
     console.log('CEP consultado:', data.cep)
     console.log('ESTADO:', data.state)
@@ -12,8 +20,10 @@ async function getcep(){
     console.log('BAIRRO:', data.neighborhood)
     console.log('RUA:', data.street)
     
+    /*Apresenta em console o erro da API (Caso ocorra algum erro)*/
 } catch (error){
     console.error(error)
 }}
 
+/*Chama a função*/
 getcep()
